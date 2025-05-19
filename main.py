766bfa8d89296b946ff5aa9860db29aac15526b6
HyperSonic0 <7951723+HyperSonic0@users.noreply.github.com>
2025-05-19 20:11:43 +0200

    done xdd

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from googlesearch import search
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import ApplicationBuilder, ContextTypes, InlineQueryHandler
 
-# client_id = "wpierdol_tu_client_id"
-# client_secret = "wpierdol_tu_client_secret"
-# telegram_token = "wpierdol_tu_telegram_token"
+client_id = "wpierdol_tu_client_id"
+client_secret = "wpierdol_tu_client_secret"
+telegram_token = "wpierdol_tu_telegram_token"
 
 def get_access_token():
     auth = f"{client_id}:{client_secret}"
